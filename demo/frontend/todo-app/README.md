2fb200fb43b189eea8f6e5bb7946ab47a4442a26
balaprasanna <balaprasannav2009@gmail.com>
2018-06-23 04:55:55 +0800

    React App completed

--- demo/frontend/todo-app/README.md
+++ demo/frontend/todo-app/README.md
@@ -1,14 +1,27 @@
 
 ### To Build
 ```
-docker build -t todoapp:0.0.1 .
+docker build -t todoapp:0.0.2 .
 ```
 
 ### To Run the container.
 ```
-docker run -d -p 80:3000 todoapp:0.0.2
+docker run -d --name todoapp -p 80:3000 todoapp:0.0.2
 ```
 
 ### Lets Explore
 *Discuss & Show*
-- how to get logs from a running container.
+- How to do live reloading of the app which is running inside a container.
+
+
+#### BY Mounting a volume inside the container, we can do live reload.
+```
+docker run -d -p 80:3000 -v `pwd`:/app todoapp:0.0.2
+```
+
+You can go to this link.
+[open http://localhost](http://localhost)
+
+
+### Continue developing..
+
--- demo/frontend/todo-app/src/App.js
+++ demo/frontend/todo-app/src/App.js
@@ -1,18 +1,86 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { ListGroup, ListGroupItem, Grid, Row, Col, Panel  } from 'react-bootstrap/lib';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    notes : [{
+      id:"1",
+      name: "No data yet",
+      content: "No data yet"
+    }],
+
+    selectedId : 0
+  }
+  componentWillMount() {
+
+    const API_ENDPOINT = "http://localhost:3000"
+
+    fetch(`${API_ENDPOINT}` + "/api/notes")
+      .then((res) => res.json())
+      .then((notes) => {
+        console.log(notes)
+        this.setState( { notes: notes } )
+      }) 
+      .catch((error) => {
+        console.log("Error, ", error)
+      })
+  }
+
+  renderListGroupItems = () => {
+    console.log(this.state)
+
+    
+  
+  }
+
+  itemClick = (index) => {
+    
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Welcome to Todo List App
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <p>  </p>
+        
+        <Grid>
+          <Row className="show-grid">
+            <Col md={4} >
+              
+              <ListGroup>
+              { 
+                this.state.notes.map((note, index) => {
+                  return <ListGroupItem href="#" key= {index}  onClick={() => { this.setState( { selectedId: index }) } }  > { note.title } </ListGroupItem>;
+                })
+              }
+              </ListGroup> 
+
+            </Col>
+
+            <Col md={8} >
+            
+              <Panel>
+                <Panel.Heading>
+                  <Panel.Title componentClass="h3"> { this.state.notes[this.state.selectedId].title } </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body> { this.state.notes[this.state.selectedId].content } </Panel.Body>
+              </Panel>
+
+            </Col>
+          </Row>
+        </Grid>
+        
+         
       </div>
     );
   }
